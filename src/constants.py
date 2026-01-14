ddce60db734246bfb05a53f980cd7404acbdcc32
SWARNIM <swarnim.tiwari@outlook.com>
2026-01-14 18:18:26 +0530

    Create constants.py

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+FEATURE_DESCRIPTIONS = {
+    "LIMIT_BAL": "Total credit limit",
+    "SEX": "Gender",
+    "EDUCATION": "Education level",
+    "MARRIAGE": "Marital status",
+    "AGE": "Client age",
+    "PAY_0": "Repayment status last month",
+    "PAY_2": "Repayment status 2 months ago",
+    "BILL_AMT1": "Last bill amount",
+    "PAY_AMT1": "Last payment amount"
+}
